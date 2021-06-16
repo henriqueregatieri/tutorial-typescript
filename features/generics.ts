19bb86b71325bb1d12acd08b9a5a7dc6e87661ed
Henrique Regatieri <riq.regatieri@gmail.com>
2021-06-16 20:46:26 -0300

    feat(features): generics

---
+++ features/generics.ts
@@ -0,0 +1,75 @@
+class ArrayOfNumbers {
+  constructor(public collection: number[]) {}
+
+  get(index: number): number {
+    return this.collection[index];
+  }
+}
+
+class ArrayOfStrings {
+  constructor(public collection: string[]) {}
+
+  get(index: number): string {
+    return this.collection[index];
+  }
+}
+
+class ArrayOfAnything<T> {
+  constructor(public collection: T[]) {}
+
+  get(index: number): T {
+    return this.collection[index];
+  }
+}
+
+new ArrayOfAnything<string>(['a', 'b']); // Best way
+new ArrayOfAnything(['a', 'b']); // Type inference (string)
+
+// Generics with functions
+
+function printStrings(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printAnything<string>(['a', 'b']); // Best way
+printAnything(['a', 'b']); // Type inference (string)
+
+// Generic Constraints
+
+class Car {
+  print() {
+    console.log("it's a car");
+  }
+}
+
+class House {
+  print() {
+    console.log("c'est une maison");
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+printHousesOrCars<House>([new House(), new House()]);
